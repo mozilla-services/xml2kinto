272daebece59bc8ed6439a16d436cfebc5052174
Tarek Ziade <tarek@ziade.org>
2015-10-27 16:58:06 +0100

    filing Kinto with the items

--- xml2kinto/main.py
+++ xml2kinto/main.py
@@ -1,5 +1,18 @@
 import os
+import hashlib
+import uuid
+
 import xml.etree.ElementTree as ET
+from kinto_client import Bucket
+from kinto_client.exceptions import KintoException
+
+
+def create_id(cert):
+    # XXX will do better later
+    hash = hashlib.md5()
+    for value in cert.values():
+        hash.update(str(value))
+    return str(uuid.UUID(hash.hexdigest()))
 
 
 DATAFILE = os.path.join(os.path.dirname(__file__), 'blocklist.xml')
@@ -7,13 +20,22 @@ URL = '{http://www.mozilla.org/2006/addons-blocklist}'
 tree = ET.parse(DATAFILE)
 root = tree.getroot()
 
-certs = []
+bucket = Bucket('default', server_url='http://localhost:8888/v1',
+                auth=('tarek', 'p4ssw0rd'))
+
+crl_collection = bucket.get_collection('onecrl')
+
 
 for item in root.iterfind('%scertItems/*' % URL):
-    cert = {'issuerName': item.get('issuerName'),
-            'serialNumber': item.get('serialNumber')}
+    cert = {}
+    for key in ('subject', 'publicKeyHash', 'serialNumber', 'issuerName'):
+        cert[key] = item.get(key)
 
-    certs.append(cert)
+    cert['id'] = create_id(cert)
 
-print certs
-
+    # can I save it at once and do a single query to kinto?
+    try:
+        record = crl_collection.create_record(cert)
+        record.save()
+    except KintoException as e:
+        raise Exception(e.response.content)
