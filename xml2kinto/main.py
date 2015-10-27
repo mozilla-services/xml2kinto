7fc1e1f04e0f921a62f9ab72330dc64b0bd27b88
Tarek Ziade <tarek@ziade.org>
2015-10-27 16:39:12 +0100

    initial xpath parsing

--- xml2kinto/main.py
+++ xml2kinto/main.py
@@ -1,5 +1,19 @@
 import os
+import xml.etree.ElementTree as ET
+
 
 DATAFILE = os.path.join(os.path.dirname(__file__), 'blocklist.xml')
+URL = '{http://www.mozilla.org/2006/addons-blocklist}'
+tree = ET.parse(DATAFILE)
+root = tree.getroot()
 
+certs = []
+
+for item in root.iterfind('%scertItems/*' % URL):
+    cert = {'issuerName': item.get('issuerName'),
+            'serialNumber': item.get('serialNumber')}
+
+    certs.append(cert)
+
+print certs
 
