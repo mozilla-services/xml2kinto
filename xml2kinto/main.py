2693db06757c1a01a4b057d08ca31cfecb145c71
Tarek Ziade <tarek@ziade.org>
2015-10-28 09:04:20 +0100

    reorganized into classes

--- xml2kinto/main.py
+++ xml2kinto/main.py
@@ -7,44 +7,59 @@ from kinto_client import Bucket
 from kinto_client.exceptions import KintoException
 
 
-def create_id(cert):
-    # XXX will do better later
-    hash = hashlib.md5()
-    for value in cert.values():
-        hash.update(str(value))
-    return str(uuid.UUID(hash.hexdigest()))
-
-
 DATAFILE = os.path.join(os.path.dirname(__file__), 'blocklist.xml')
-URL = '{http://www.mozilla.org/2006/addons-blocklist}'
-tree = ET.parse(DATAFILE)
-root = tree.getroot()
 user = 'mark'
-
 permissions = {'read': ["system.Everyone"]}
-
-bucket = Bucket('onecrl', server_url='http://localhost:8888/v1',
-                auth=(user, 'p4ssw0rd'), create=True,
-                permissions=permissions)
-
-colls = bucket.list_collections()
-
-if 'blocklist' not in colls:
-    bucket.create_collection('blocklist')
-
-crl_collection = bucket.get_collection('blocklist')
+bucket_name = 'onecrl'
+collection_name = 'blocklist'
+kinto_server = 'http://localhost:8888/v1'
 
 
-for item in root.iterfind('%scertItems/*' % URL):
+class KintoRecords(object):
+    def __init__(self):
+        self.bucket = Bucket(bucket_name, server_url=kinto_server,
+                             auth=(user, 'p4ssw0rd'), create=True,
+                             permissions=permissions)
+        colls = self.bucket.list_collections()
+
+        if collection_name not in colls:
+            self.bucket.create_collection(collection_name)
+        self.collection = self.bucket.get_collection(collection_name)
+
+    def create_record(self, data):
+        if 'id' not in data:
+            data['id'] = self._create_id(data)
+        rec = self.collection.create_record(data)
+        rec.save()   # XXX
+        return rec
+
+    def _create_id(self, data):
+        hash = hashlib.md5()
+        for value in cert.values():
+            hash.update(str(value))
+        return str(uuid.UUID(hash.hexdigest()))
+
+
+class XMLRecords(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.url = '{http://www.mozilla.org/2006/addons-blocklist}'
+        self.tree = ET.parse(self.filename)
+        self.root = self.tree.getroot()
+        self.records = self.root.iterfind('%scertItems/*' % self.url)
+
+
+
+xml = XMLRecords(DATAFILE)
+kinto = KintoRecords()
+
+
+for record in xml.records:
     cert = {}
     for key in ('subject', 'publicKeyHash', 'serialNumber', 'issuerName'):
-        cert[key] = item.get(key)
+        cert[key] = record.get(key)
 
-    cert['id'] = create_id(cert)
-
-    # can I save it at once and do a single query to kinto?
     try:
-        record = crl_collection.create_record(cert)
-        record.save()
+        kinto.create_record(cert)
     except KintoException as e:
         raise Exception(e.response.content)
